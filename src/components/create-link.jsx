c78c350c74695f15ac2b6422ddf809afa71f6423
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-08-15 01:10:31 +0100

    url upload api setuo

---
+++ src/components/create-link.jsx
@@ -0,0 +1,8 @@
+
+const CreateLink = () => {
+  return (
+    <div>CreateLink</div>
+  )
+}
+
+export default CreateLink
--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
     const {data, error} = await supabase
@@ -14,7 +14,6 @@ export async function getUrls(user_id) {
     return data
 }
 
-
 export async function deleteUrl(id) {
     const {data, error} = await supabase
         .from('urls')
@@ -24,6 +23,36 @@ export async function deleteUrl(id) {
     if(error) {
         console.error(error.message);
         throw new Error("Unable to delete URLs");
+    }
+
+    return data
+}
+
+export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
+    const short_url = Math.random().toString(36).substring(2,6);
+    const fileName = `qr-${short_url}`;
+    const {error: storageError} = await supabase.storage.from('qrs').upload(fileName, qrcode)
+
+    if(storageError) throw new Error(storageError.message);
+
+    const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`
+
+    const {data, error} = await supabase
+        .from('urls')
+        .insert([
+            {
+                title,
+                original_url: longUrl,
+                custom_url: customUrl || null,
+                user_id,
+                short_url,
+                qr,
+            },
+        ]).select();
+    
+    if(error) {
+        console.error(error.message);
+        throw new Error("Error creating short URL");
     }
 
     return data
