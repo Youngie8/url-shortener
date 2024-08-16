7373b90637a30cf50007b7c0d30e5e57f6c15cd5
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-08-16 04:02:06 +0100

    create-link done

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -27,20 +27,20 @@ const LinkCard = ({url, fetchUrls}) => {
     <div className="flex flex-col md:flex-row gap-5 border p-2 rounded-lg">
         <img src={url.qr} alt="QR code" className="h-32 object-contain ring ring-#36d7b7 self-start"/>
         <Link to={`/link/${url?.id}`} className="flex flex-col flex-1">
-            <span className="text-3xl font-extrabold hover:underline cursor-pointer"> {url?.title}</span>
-            <span className=" text-2xl text-blue-400 font-bold hover:underline cursor-pointer">https://scissor.com/{url?.custom_url? url?.custom_url : url.short_url}</span>
+            <span className="text-2xl font-extrabold hover:underline cursor-pointer"> {url?.title}</span>
+            <span className=" text-xl text-blue-400 font-bold hover:underline cursor-pointer">https://scissor-oluwole.netlify.app/{url?.custom_url? url?.custom_url : url.short_url}</span>
             <span className="flex items-center gap-1 hover:underline cursor-pointer">{url?.original_url}</span>
             <span className="flex items-end font-extralight text-sm flex-1 text-gray-400">{new Date(url?.created_at).toLocaleString()}</span>
         </Link>
 
         <div className="flex gap-2">
-            <Button variant="ghost" onclick={() => navigator.clipboard.writeText(`https://scissor.com/${url?.short_url}`)}>
+            <Button variant="ghost" onClick={() => navigator.clipboard.writeText(`https://scissor-oluwole.netlify.app/${url?.short_url}`)}>
                 <Copy/>
             </Button>
             <Button variant="ghost" onClick={downloadImage}>
                 <Download/>
             </Button>
-            <Button variant="ghost" onClick={() => fnDelete().then(() => fetchUrls())}>
+            <Button className='text-red-600' variant='ghost' onClick={() => fnDelete().then(() => fetchUrls())}>
                 {loadingDelete? <BeatLoader size={4} color="red"/> : <Trash2/>}
             </Button>
         </div>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -16,6 +16,7 @@ import { getUrls } from '@/db/apiUrls'
 import { UrlState } from '@/context'
 import { getClicksForUrls } from '@/db/apiClicks'
 import LinkCard from '@/components/link-card'
+import CreateLink from '@/components/create-link'
 
 
 const Dashboard = () => {
@@ -65,7 +66,7 @@ const Dashboard = () => {
       </div>
       <div className="flex justify-between">
         <h1 className='text-3xl font-bold'>My Links</h1>
-        <Button>Create Link</Button>
+        <CreateLink/>
       </div>
       <div className="relative">
         <Input
