a109baca5f988dd13854034fd9e383ac49b59aa1
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-08-16 12:11:32 +0100

    all functionalities done

--- src/components/device-stats.jsx
+++ src/components/device-stats.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { PieChart, Pie, ResponsiveContainer, Cell } from "recharts";
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const DeviceStats = ({stats}) => {
+export default function DeviceStats({stats}) {
+    const deviceCount = stats.reduce((acc, item) => {
+        if(!acc[item.device]) {
+            acc[item.device] = 0;
+        } 
+        acc[item.device]++;
+
+        return acc;
+    }, {});
+
+    const result = Object.entries(deviceCount).map(([device, count]) => ({
+        device,
+        count: deviceCount[device],
+    }));
+
   return (
-    <div>DeviceStats</div>
-  )
+    <div style={{width:'100%', height: 300}}>
+        <ResponsiveContainer>
+            <PieChart width={700} height={400} className="self-center">
+                <Pie 
+                    data={result} 
+                    labelLine={false} 
+                    label={({device, percent}) => `${device}: ${(percent * 100).toFixed(0)}%`} 
+                    dataKey="count" />
+                    {result.map((_, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                
+            </PieChart>
+        </ResponsiveContainer>
+    </div>
+  );
 }
-
-export default DeviceStats
--- src/components/location-stats.jsx
+++ src/components/location-stats.jsx
@@ -13,19 +13,28 @@ import {
 
 export default function Location({stats}) {
     const cityCount = stats.reduce((acc, item) => {
-        acc[item.city] = 1;
-    }, {})
+        if(acc[item.city]) {
+            acc[item.city] += 1;
+        } else acc[item.city] = 1;
+
+        return acc;
+    }, {});
+
+    const cities = Object.entries(cityCount).map(([city, count]) => ({
+        city,
+        count,
+    }));
   return (
     <div style={{width:'100%', height: 300}}>
         <ResponsiveContainer>
-        <LineChart width={700} height={300} data={data}>
-            <XAxis dataKey="name" />
+        <LineChart width={700} height={300} data={cities.slice(0, 5)}>
+            <XAxis dataKey="city" padding={{ left: 30, right: 30 }}/>
             <YAxis />
             <Tooltip labelStyle={{color: 'green'}}/>
             <Legend />
             <Line
                 type="monotone"
-                dataKey="pv"
+                dataKey="count"
                 stroke="#8884d8"
             />
         </LineChart>
